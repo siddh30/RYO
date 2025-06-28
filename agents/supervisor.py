896b87ef5e4b07475a7ed2b1663afaa031efcfd1
Siddharth <siddh30@gmail.com>
2025-06-28 03:55:29 -0400

    Adding Search Agent and imporving state of Supervisor

--- agents/supervisor.py
+++ agents/supervisor.py
@@ -1,18 +1,29 @@
 import sys
 sys.path.append('../')
-
+import pandas as pd
 from langgraph_supervisor import create_supervisor
 from langgraph.checkpoint.memory import InMemorySaver
 from langchain_core.prompts import PromptTemplate
+from langgraph_supervisor.handoff import create_forward_message_tool
+
+######### Import Agents #########
+from agents.news_agent import news_agent
+from agents.central_memory_agent import central_memory_agent
 
 ###### From config #######
 from config import Config
 conf = Config()
 
-import pandas as pd
 
-from agents.news_agent import news_agent
-from agents.central_memory_agent import central_memory_agent
+######## Resource Manager ########
+from utils.resource_manager import ResourceManager
+resource_manager = ResourceManager.get_instance()
+
+
+
+prompt_blueprint = resource_manager.prompt_loader("supervisor_prompt")
+
+
 
 try:
     long_term_memory = pd.read_csv(f"{conf.memory_path}/Permanent_Memory.csv")
@@ -30,28 +41,19 @@ except:
     reminders_str = ""
 
 
-prompt_template = PromptTemplate.from_template("You are Ryo, health and lifestyle supervisor managing a Central Memory Agent and a News Agent. You will be also provided with Past Interaction asked by the user store permanently and as reminders.\n" 
-                                "\n<Agent Instructions>\n"
-                                "1. For news related queries use News Agent.\n"
-                                "2. For storing memories and reminders, use Central Memory Agent. \n" 
-                                "\n<Past Interactions stored as Permanent Memory>\n" 
-                                "{long_term_memory_string}\n"
-                                "\n<Past Interactions stored as Reminders Memory>\n" 
-                                "{reminder_string}")
-prompt = prompt_template.invoke({"long_term_memory_string": long_term_memory_str,
+prompt_template = PromptTemplate.from_template(prompt_blueprint)
+prompt = prompt_template.invoke({"ToolMessage": '{"tool":"forwarded_response","args":{"from_agent":"news_agent"}}',
+                            "long_term_memory_string": long_term_memory_str,
                             "reminder_string": reminders_str}).to_string()
 
+
+forwarding_tool = create_forward_message_tool("forwarded_response")
 
 graph = create_supervisor([news_agent, central_memory_agent], 
                         model=conf.model, 
                         supervisor_name='Ryo',
-                        prompt=prompt
-                        
-    
-    
-)
+                        prompt=prompt,
+                        tools=[forwarding_tool],
+                        output_mode='last_message')
+
 ryo = graph.compile(checkpointer=InMemorySaver())
-ryo
-
-
-
